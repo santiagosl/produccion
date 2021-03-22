5020ac7359a5b33330276e29c4e337a9ee8f5026
Santiago <santiagosl314@hotmail.com>
2021-03-22 15:48:56 +0100

    Area de clientes, tabla resumen de cliente y boton de alta clients con otra vista

---
+++ src/Controller/ClientesController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Cliente;
+
+class ClientesController extends AbstractController {
+    
+    /**
+     * @Route("/clientes", name="clientes");
+     */
+
+    public function clientes() { 
+
+    $repositorio = $this->getDoctrine()->getRepository(Cliente::class); 
+    $clientes = $repositorio->findAll();
+    return $this->render('clientes.html.twig' ,array ('clientes' => $clientes));
+    }
+}
+
+
+?>
+
+
+
+        
